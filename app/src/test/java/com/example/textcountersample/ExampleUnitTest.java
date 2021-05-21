fd9dd3c85833f16ef5ad75651e033b2c9aef2ca2
monikabar <monikabarkauskaite@gmail.com>
2021-05-21 20:11:45 +0300

    Sukurti chars testai

--- app/src/test/java/com/example/textcountersample/ExampleUnitTest.java
+++ app/src/test/java/com/example/textcountersample/ExampleUnitTest.java
@@ -2,6 +2,7 @@ package com.example.textcountersample;
 
 import org.junit.Test;
 
+import static com.example.textcountersample.utils.ElementsCalculator.getCharsCount;
 import static org.junit.Assert.*;
 
 /**
@@ -11,7 +12,45 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void whenCharsInputEmpty() {
+        String givenInput = "";
+        int expectedResult = 0;
+        int actualResult = getCharsCount(givenInput);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void whenCharsInputLetters() {
+        String givenInput = "abcd";
+        int expectedResult = 4;
+        int actualResult = getCharsCount(givenInput);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void whenCharsInputNumbers() {
+        String givenInput = "12";
+        int expectedResult = 2;
+        int actualResult = getCharsCount(givenInput);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void whenCharsInputSymbols() {
+        String givenInput = "@./";
+        int expectedResult = 3;
+        int actualResult = getCharsCount(givenInput);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void whenCharsInputSpace() {
+        String givenInput = " ";
+        int expectedResult = 1;
+        int actualResult = getCharsCount(givenInput);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void whenCharsInputVarious() {
+        String givenInput = "A1_b@ &";
+        int expectedResult = 7;
+        int actualResult = getCharsCount(givenInput);
+        assertEquals(expectedResult, actualResult);
     }
 }
